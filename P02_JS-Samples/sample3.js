380cf3f0ef379c088c0d3e6ae2756cc6f6c6c190
Arı Bilgi <nomail@aribilgi.com.tr>
2024-04-04 10:17:54 +0300

    Some changes

---
+++ P02_JS-Samples/sample3.js
@@ -0,0 +1,13 @@
+﻿function sayiAl() {
+    var sayi1 = parseInt(document.getElementById("sayi1").value);
+
+    var sayi2 = parseInt(document.getElementById("sayi2").value);
+
+    var toplam1 = sayi1 + sayi2; // normal toplama işlemi
+
+    var toplam2 = sayi1 + " - " + sayi2; // karaktersel birleştirme
+
+    document.getElementById("sonuc1").innerHTML = toplam1;
+
+    document.getElementById("sonuc2").innerHTML = toplam2;
+}
