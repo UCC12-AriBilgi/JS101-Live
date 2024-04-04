dfdceca8410aa2729b5bbf47fb94131713b5c39c
Arı Bilgi <nomail@aribilgi.com.tr>
2024-04-04 13:53:26 +0300

    Some changes

---
+++ P02_JS-Samples/sample16.js
@@ -0,0 +1,21 @@
+﻿function tikla() {
+    document.getElementById("sonuca").innerHTML = "Butona tıklandı..";
+}
+
+function mesajover() {
+    document.getElementById("sonucb").innerHTML = "Butonun üzerindeyim..";
+
+}
+
+function mesajout() {
+    document.getElementById("sonucb").innerHTML = "Butonun üzerinden çekildim..";
+
+}
+
+function mesajbasili() {
+    document.getElementById("sonucc").innerHTML = "Butona basılıyor..";
+}
+
+function mesajbasilidegil() {
+    document.getElementById("sonucc").innerHTML = "Butona artık basılmıyor..";
+}
