5427fd8a5a4ff9af03dd840e0e661b2d1a449db8
Arı Bilgi <nomail@aribilgi.com.tr>
2024-04-04 10:32:51 +0300

    Some changes

---
+++ P02_JS-Samples/sample04.js
@@ -0,0 +1,39 @@
+﻿function toplama() {
+    var sayi1 = parseInt(document.getElementById("sayi1").value);
+
+    var sayi2 = parseInt(document.getElementById("sayi2").value);
+
+    var sonuc = sayi1 + sayi2;
+
+    document.getElementById("sonuc").innerHTML = sonuc;
+}
+
+function cikarma() {
+    var sayi1 = parseInt(document.getElementById("sayi1").value);
+
+    var sayi2 = parseInt(document.getElementById("sayi2").value);
+
+    var sonuc = sayi1 - sayi2;
+
+    document.getElementById("sonuc").innerHTML = sonuc;
+}
+
+function carpma() {
+    var sayi1 = parseInt(document.getElementById("sayi1").value);
+
+    var sayi2 = parseInt(document.getElementById("sayi2").value);
+
+    var sonuc = sayi1 * sayi2;
+
+    document.getElementById("sonuc").innerHTML = sonuc;
+}
+
+function bolme() {
+    var sayi1 = parseInt(document.getElementById("sayi1").value);
+
+    var sayi2 = parseInt(document.getElementById("sayi2").value);
+
+    var sonuc = sayi1 / sayi2;
+
+    document.getElementById("sonuc").innerHTML = sonuc;
+}
