3bd961acd92b51d766845d9ed902e80d92a132ac
Arı Bilgi <nomail@aribilgi.com.tr>
2024-04-04 11:08:16 +0300

    Some changes

---
+++ P02_JS-Samples/sample05.js
@@ -0,0 +1,9 @@
+﻿function degistir() {
+    var element = document.getElementsByClassName("yazi"); // dokumandaki yazi class tanımı yapılmış tüm elementlerin hepsini bir anda secer.
+
+
+    for (var index = 0;  index < element.length; index++) {
+        element[index].style.color = "orange";
+        element[index].style.fontSize = "26px";
+    }
+}
