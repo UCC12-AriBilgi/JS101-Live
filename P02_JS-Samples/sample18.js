9ff158527d2e297e54fda778cbdaab7fc3f63cf5
Arı Bilgi <nomail@aribilgi.com.tr>
2024-04-04 14:37:51 +0300

    Some changes

---
+++ P02_JS-Samples/sample18.js
@@ -0,0 +1,50 @@
+﻿function gengoster() {
+    var genislik = document.getElementById("tablo").getAttribute("width");
+
+    document.getElementById("kutu1").value = genislik;
+}
+
+function genayarla() {
+    // kutu1 içine girilen degerele ayarlama
+    var genislik = document.getElementById("kutu1").value;
+
+    document.getElementById("tablo").setAttribute("width", genislik);
+}
+
+function renkgoster() {
+    var renk = document.getElementById("tablo").getAttribute("bgcolor");
+
+    document.getElementById("kutu2").value = renk;
+}
+
+function renkayarla1() {
+    var renk = document.getElementById("kutu2").value;
+
+    document.getElementById("tablo").setAttribute("bgcolor", renk);
+}
+
+function renkayarla2() {
+    var renk = document.getElementById("secim").value;
+
+    document.getElementById("tablo").setAttribute("bgcolor", renk);
+}
+
+function genazalt() {
+    var genislik = parseInt(document.getElementById("tablo").getAttribute("width"));
+
+    genislik = genislik - 25;
+
+    document.getElementById("tablo").setAttribute("width", genislik);
+
+    document.getElementById("kutu1").value = genislik;
+}
+
+function genarttir() {
+    var genislik = parseInt(document.getElementById("tablo").getAttribute("width"));
+
+    genislik += 25;
+
+    document.getElementById("tablo").setAttribute("width", genislik);
+
+    document.getElementById("kutu1").value= genislik;
+}
