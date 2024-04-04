aef1e131e28af5b50002578e79a26b3ca42ebd6b
Arı Bilgi <nomail@aribilgi.com.tr>
2024-04-04 14:04:12 +0300

    Some changes

---
+++ P02_JS-Samples/sample17.js
@@ -0,0 +1,9 @@
+﻿function mesajover() {
+    document.getElementById("sonuc").innerHTML = "Resim üzerine geldim..";
+
+}
+
+function mesajout() {
+    document.getElementById("sonuc").innerHTML = "Resim üzerinden ayrıldım..";
+
+}
