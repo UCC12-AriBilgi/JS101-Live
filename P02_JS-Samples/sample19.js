7a4ff560248d69ef5be97e3ae060a4023a55e5d1
Arı Bilgi <nomail@aribilgi.com.tr>
2024-04-04 14:58:20 +0300

    Some changes

---
+++ P02_JS-Samples/sample19.js
@@ -0,0 +1,19 @@
+﻿// Sayfa yüklendiğinde menu divinin kapalı olarak gelmesini gostermek için
+var menuDurum = 0; // menu kapalı
+
+function menuDegistir() {
+    var menuObj = document.getElementById("menu");
+
+    if (menuDurum == 0) {
+        // demekki menu kapalı onu aç..
+        menuObj.style.display = "block";
+        menuDurum = 1; // menu acık
+
+    }
+    else {
+        //menu acıksa kapat
+        menuObj.style.display = "none";
+        menuDurum = 0; // menu acık
+    }
+
+}
